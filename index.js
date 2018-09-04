655acf9cf48c5a41acc784e198cb750dc19932db
Chris Hanson <blondyhanson@gmail.com>
2018-09-04 12:42:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function happyHolidays() {
 return("Happy holidays!")
 }
 
-function happyHolidaysTo() {
+function happyHolidaysTo(name) {
   return("Happy holidays, ${name}!")
 }
 
-function happyHolidayTo() {
+function happyHolidayTo(name) {
   return("Happy ${holiday}, ${name}!")
 }
 
-function holidayCountdown() {
+function holidayCountdown(name) {
   return("It's ${days} days until ${holiday}!")
 }
